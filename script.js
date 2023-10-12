9bd9072c14a274dee996b3d3cf8f220441f9d3ae
bridau19 <bridau19@gmail.com>
2023-10-12 02:21:26 -0400

    added cheat

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var slider = document.getElementById("myRange");
+var slider2 = document.getElementById("myRange2");
 var glasses = document.getElementById("glasses");
 glasses.style.setProperty("left", slider.value + "px");
+slider2.style.transform = "rotate(90deg)";
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
   glasses.style.setProperty("left", this.value + "px");
+};
+slider2.oninput = function () {
+  glasses.style.setProperty("top", this.value + "px");
 };
 
 function showGlasses() {
@@ -12,8 +17,8 @@ function showGlasses() {
 }
 
 function move() {
-  var current = document.getElementById("glasses").style.marginLeft;
-  document.getElementById("glasses").style.marginLeft = current + 100 + "px";
+  var current = document.getElementById("glasses").style.marginTop;
+  document.getElementById("glasses").style.marginTop = current + 150 + "px";
 }
 function move1() {
   var current = document.getElementById("glasses").style.marginTop;
@@ -31,19 +36,23 @@ function move4() {
   document.getElementById("glasses").style.marginTop = current + 320 + "px";
 }
 
+function reveal() {
+  document.getElementById("myRange2").style.display = "block";
+}
+
 function change() {
   var current = document.getElementById("glasses").style.marginTop;
   if (document.getElementById("check-one").checked) {
-    document.getElementById("glasses").style.marginTop = current + 320 + "px";
+    document.getElementById("glasses").style.marginTop = current + 100 + "px";
   } else {
-    document.getElementById("glasses").style.marginTop = current + 320 + "px";
+    document.getElementById("glasses").style.marginTop = current + 100 + "px";
   }
 }
 
 function changeUp() {
   var current = document.getElementById("glasses").style.top;
   if (document.getElementById("check-two").checked) {
-    document.getElementById("glasses").style.top = current - 300 + "px";
+    document.getElementById("glasses").style.top = current - 50 + "px";
   } else {
     document.getElementById("glasses").style.top = current + 320 + "px";
   }
@@ -53,9 +62,9 @@ function randomBox() {
   var current = document.getElementById("glasses").style.top;
   if (document.getElementById("check-three").checked) {
     document.getElementById("glasses").style.top =
-      current - Math.random() * 1000 + "px";
+      current - Math.random() * 100 + "px";
   } else {
     document.getElementById("glasses").style.top =
-      current + Math.random() * 1000 + "px";
+      current + Math.random() * 100 + "px";
   }
 }
