b37505bb4c2fb4b89fbd8077fc9c21a04a2f6023
bridau19 <bridau19@gmail.com>
2023-10-12 14:50:05 -0400

    added interaction

--- script.js
+++ script.js
@@ -16,55 +16,73 @@ function showGlasses() {
   document.getElementById("glasses").style.display = "block";
 }
 
-function move() {
-  var current = document.getElementById("glasses").style.marginTop;
-  document.getElementById("glasses").style.marginTop = current + 150 + "px";
-}
-function move1() {
-  var current = document.getElementById("glasses").style.marginTop;
-  document.getElementById("glasses").style.marginTop = current - 200 + "px";
-}
-function move2() {
-  var current = document.getElementById("glasses").style.marginTop;
-  document.getElementById("glasses").style.marginTop = current + 100 + "px";
-}
-function move3() {
-  document.getElementById("glasses").style.marginTop = "400px";
-}
-function move4() {
-  var current = document.getElementById("glasses").style.marginTop;
-  document.getElementById("glasses").style.marginTop = current + 320 + "px";
+function showBox() {
+  document.getElementById("reveal").style.display = "block";
 }
 
 function reveal() {
-  document.getElementById("myRange2").style.display = "block";
+  if (document.getElementById("reveal").checked) {
+    document.getElementById("myRange2").style.display = "block";
+  } else {
+    document.getElementById("myRange2").style.display = "none";
+  }
+}
+function showButton() {
+  document.getElementById("myButton").style.display = "block";
 }
 
 function change() {
-  var current = document.getElementById("glasses").style.marginTop;
-  if (document.getElementById("check-one").checked) {
-    document.getElementById("glasses").style.marginTop = current + 100 + "px";
-  } else {
-    document.getElementById("glasses").style.marginTop = current + 100 + "px";
-  }
+  document.getElementById("glasses").style.top = "600px";
 }
 
-function changeUp() {
-  var current = document.getElementById("glasses").style.top;
-  if (document.getElementById("check-two").checked) {
-    document.getElementById("glasses").style.top = current - 50 + "px";
-  } else {
-    document.getElementById("glasses").style.top = current + 320 + "px";
-  }
+function change2() {
+  glasses.style.top = Math.random() * 100 + "px";
 }
 
-function randomBox() {
-  var current = document.getElementById("glasses").style.top;
-  if (document.getElementById("check-three").checked) {
-    document.getElementById("glasses").style.top =
-      current - Math.random() * 100 + "px";
-  } else {
-    document.getElementById("glasses").style.top =
-      current + Math.random() * 100 + "px";
-  }
+function change3() {
+  glasses.style.top = Math.random() * 150 + "px";
 }
+
+function change4() {
+  var getNum = glasses.style.top.replace("px", "");
+  var convertNum = parseInt(getNum, 10);
+  convertNum = convertNum + 25;
+
+  glasses.style.top = convertNum + `px`;
+  console.log("was: ", getNum, "now: ", convertNum);
+}
+
+function change5() {
+  document.getElementById("glasses").style.top = "40vh";
+}
+
+function change6() {
+  var getNum = glasses.style.top.replace("px", "");
+  var convertNum = parseInt(getNum, 10);
+  convertNum = convertNum - 15;
+
+  glasses.style.top = convertNum + `px`;
+  console.log("was: ", getNum, "now: ", convertNum);
+}
+
+// function change() {
+//   var1 = document.getElementById("radio1");
+//   var2 = document.getElementById("radio2");
+//   var3 = document.getElementById("radio3");
+//   var4 = document.getElementById("radio4");
+//   var5 = document.getElementById("radio5");
+//   var6 = document.getElementById("radio6");
+//   if (var1.selected == true) {
+//     glasses.style.top = "900px";
+//   } else if (var2.selected === true) {
+//     glasses.style.top = "200px";
+//   } else {
+//     glasses.style.top = "0px";
+//   }
+// }
+
+// function change() {
+//   var current = document.getElementById("glasses").style.top;
+//   document.getElementById("glasses").style.top =
+//     current + Math.random() * 100 + "px";
+// }
